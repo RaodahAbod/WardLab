314e6fdc2d5d4053b78547a304ce4665b5cd0338
RaodahAbod <raodah.abod@gmail.com>
2024-01-20 08:48:04 -0600

    removed old autosomal function, changed input to workingFile

--- Clean_and_Visualize.R
+++ Clean_and_Visualize.R
@@ -1,5 +1,5 @@
 rm(list = ls())
-setwd("C:/Users/raoda/OneDrive/Desktop/R Stuff/CUT&Tag Processing")
+setwd("C:/Users/raoda/Desktop/R Stuff/CUT&Tag Processing")
 
 # Load Data ---------------------------------------------------------------
 loadFile <- function(){
@@ -7,37 +7,12 @@ loadFile <- function(){
   return(file)
 }
 
-day0_H <- loadFile()
-day0h_H <- loadFile()
-day1_H <- loadFile()
-day2_H <- loadFile()
-day3_H <- loadFile()
-day4_H <- loadFile()
-day5_H <- loadFile()
-day7_H <- loadFile()
-day10_H <- loadFile()
-day15_H <- loadFile()
-day30_H <- loadFile()
+workingFile <- loadFile()
 
 dataName <- readline(prompt = "Please enter the full Data Name: ")
 autoDataName <- paste("autosomal",dataName)
 
 # Removing Non Autosomal Data ---------------------------------------------
-nonautosomal <- function(histonemod_day) {
-  test <- subset(histonemod_day,histonemod_day$V1 == "chr1" | histonemod_day$V1 == "chr2" |
-                   histonemod_day$V1 == "chr3" | histonemod_day$V1 == "chr4" |
-                   histonemod_day$V1 == "chr5" | histonemod_day$V1 == "chr6"  |
-                   histonemod_day$V1 == "chr7" | histonemod_day$V1 == "chr8" |
-                   histonemod_day$V1 == "chr9" | histonemod_day$V1 == "chr10"  |
-                   histonemod_day$V1 == "chr11" | histonemod_day$V1 == "chr12" |
-                   histonemod_day$V1 == "chr13" | histonemod_day$V1 == "chr14"  |
-                   histonemod_day$V1 == "chr15" | histonemod_day$V1 == "chr16" |
-                   histonemod_day$V1 == "chr17" | histonemod_day$V1 == "chr18"  |
-                   histonemod_day$V1 == "chr19" | histonemod_day$V1 == "chr20" |
-                   histonemod_day$V1 == "chr21" | histonemod_day$V1 == "chr22")
-  return(test)
-}
-
 nonautosomalNew <- function(histonemod_day) {
   test <- histonemod_day %>%
     select(-contains("chrUn"), -contains("chrX"), -contains("chrY"), -contains("chrM"))
@@ -48,6 +23,14 @@ orig <- nonautosomal(workingFile)
 new <- nonautosomalNew(workingFile)
 
 autos_workingFile <- nonautosomal(workingFile)
+autos_NewWorkingFile <- nonautosomalNew(workingFile)
+
+# Save FIltered Autosomals ------------------------------------------------
+
+newFileName <- readline(prompt = "Enter the name for new file: ")
+newFileName <- paste0(newFileName,".bed")
+write.table(autos_workingFile, newFileName ,row.names = F ,col.names = F ,
+            sep="\t" , quote=FALSE)
 
 # Number of Peaks in Samples ----------------------------------------------
 numPeaks <- nrow(workingFile)
@@ -93,12 +76,7 @@ hist(autos_workingFile$PeakLength, main = paste("Distribution of Peak Length in"
 hist(autos_workingFile$V7, main = paste("Distribution of Peak Intensity in", dataName),
      xlim = c(0,100), breaks = 50, xlab = "Peak Intensity")
 
-# Save FIltered Autosomals ------------------------------------------------
 
-newFileName <- readline(prompt = "Enter the name for new file: ")
-newFileName <- paste0(newFileName,".bed")
-write.table(autos_workingFile, newFileName ,row.names = F ,col.names = F ,
-            sep="\t" , quote=FALSE)
 
 
 
