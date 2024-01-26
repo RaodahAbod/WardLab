7fb15980c4a739f4f4260857dde238b2222ee48e
RaodahAbod <raodah.abod@gmail.com>
2024-01-26 15:55:36 -0600

    File serves to split peaks in bed file into the top specified peak cutoffs ordered by peak intensity

--- Split_to_Quartiles.R
+++ Split_to_Quartiles.R
@@ -1,102 +1,43 @@
 rm(list = ls())
 setwd("C:/Users/raoda/OneDrive/Desktop/R Stuff/CUT&Tag Processing")
 
-library("tidyverse")
-library("readxl")
-
-# Load Data -------------------------------------------------------------------------
-
+#  Load File ------------------------------------------------------------------------
 workingData <- read.table(choose.files())
 
-# Section into Quartiles.  ----------------------------------------------------------
+# Split into Top groups -------------------------------------------------------------
+#I need a top 1/4th, top 1/3rd, top 1/2, and top 2/3 :D
+#get the criteria for the top whatever then use the select function (i think) to separate 
+#based on equal to or greater than the critera
 
-quartilesRoad <- function(dataSet){
-   firstQ <- quantile(dataSet$V5, 0.25)
-   secondQ <- quantile(dataSet$V5, 0.5)
-   thirdQ <- quantile(dataSet$V5, 0.75)
-   
-   Percent25 <- subset(dataSet, dataSet$V5 <= firstQ)
-   Percent50 <- subset(dataSet, dataSet$V5 > firstQ & dataSet$V5 <= secondQ)
-   Percent75 <- subset(dataSet, dataSet$V5 > secondQ & dataSet$V5 <= thirdQ)
-   Percent100 <- subset(dataSet, dataSet$V5 > thirdQ)
-   
-   sortedQuartiles <- list(Percent25,Percent50,Percent75,Percent100)
-   return(sortedQuartiles)
-}
-quartiles <- function(dataSet){
-   firstQ <- quantile(dataSet$V4, 0.25)
-   secondQ <- quantile(dataSet$V4, 0.5)
-   thirdQ <- quantile(dataSet$V4, 0.75)
-   
-      Percent25 <- subset(dataSet, dataSet$V4 <= firstQ)
-      Percent50 <- subset(dataSet, dataSet$V4 > firstQ & dataSet$V4 <= secondQ)
-      Percent75 <- subset(dataSet, dataSet$V4 > secondQ & dataSet$V4 <= thirdQ)
-      Percent100 <- subset(dataSet, dataSet$V4 > thirdQ)
-      
-   sortedQuartiles <- list(Percent25,Percent50,Percent75,Percent100)
-   return(sortedQuartiles)
+separatePeaks <- function(peak_file,topWhatever){
+   sorted <- arrange(peak_file,-V7)
+   index <- floor(length(sorted[,1])*topWhatever)
+   #top_criteria <- peak_file$V5[index]
+   topPeaks <- sorted[1:index,]
+   #topPeaks <- sorted[index:length(sorted[,1]),]
+   #topPeaks <- subset(sorted, sorted >= top_criteria)
+   return(topPeaks)
 }
 
-quartiled_data <- quartilesRoad(workingData)
+topHalf_Peaks <- separatePeaks(workingData, 1/2) 
+topThird_Peaks <- separatePeaks(workingData, 1/3) 
+topFourth_Peaks <- separatePeaks(workingData, 1/4)
+topTwoThird_Peaks <- separatePeaks(workingData, 2/3)
 
-quartiled_data <- quartiles(workingData)
-Q1 <- quartiled_data[[1]]
-Q2 <- quartiled_data[[2]]
-Q3 <- quartiled_data[[3]]
-Q4 <- quartiled_data[[4]]
+# Save Grouped Peaks ----------------------------------------------------------------
 
-# Save Quartile Information ---------------------------------------------------------
 setwd("C:/Users/raoda/OneDrive/Desktop/R Stuff/CUT&Tag Processing/Quartiled Bed Files")
 
+
 saveQuartileFile <- function(dataset){
-newFileName <- readline(prompt = "Enter the name for new file: ")
-newFileName <- paste0(newFileName,".bed")
-write.table(dataset, newFileName ,row.names = F ,col.names = F ,
-            sep="\t" , quote=FALSE) 
+   newFileName <- readline(prompt = "Enter the name for new file: ")
+   newFileName <- paste0(newFileName,".bed")
+   write.table(dataset, newFileName ,row.names = F ,col.names = F ,
+               sep="\t" , quote=FALSE) 
 }
 
-saveQuartileFile(Q1)
-saveQuartileFile(Q2)
-saveQuartileFile(Q3)
-saveQuartileFile(Q4)
-# Simple Split into Quartiles using new tricks -------------------------------------------------------
-
-addQuartiles <- function(dataSet){
-   firstQ <- quantile(dataSet$V4)
-   secondQ <- quantile(dataSet$V4)
-   thirdQ <- quantile(dataSet$V4)
-   
-   dataSet %>%
-      dplyr::select(V1,V2, V3, V4) %>%
-      mutate(Quartiles = if_else(V4 <= firstQ[2,1],0.25, 
-                                 if_else(V4 > quantile(dataSet$V4,0.25)&& V4 <= quantile(dataSet$V4,0.5),0.5, 
-                                         if_else(V4 > quantile(dataSet$V4,0.5)&& V4 <= quantile(dataSet$V4,0.75), 0.75),1.00))) -> QuartileData
-   
-   return(QuartileData)
-}
-
-data_two_quartiles <- addQuartiles(DataSet_two)
-
-
-#doesnt work. 
-# test <- function(dataSet){ 
-#    firstQ <- quantile(dataSet$V4, 0.25)
-#    secondQ <- quantile(dataSet$V4, 0.5)
-#    thirdQ <- quantile(dataSet$V4, 0.75)
-#    
-#    ifelse(dataSet$V4 <= firstQ, dataSet$V5 <- 0.25, 
-#           ifelse(dataSet$V4 > firstQ & dataSet$V4 <= secondQ, dataSet$V5 <- 0.5,
-#                  ifelse(dataSet$V4 > secondQ & dataSet$V4 <= thirdQ, dataSet$V5 <- 0.75,
-#                         NA)))
-#    return(dataSet)
-#    
-#    }
-
-
-
-#put these lines of code into the beginning of the chipseeker script
-# Q1 <- quartile_dataOne[[1]]
-# Q2 <- quartile_dataOne[[2]]
-# Q3 <- quartile_dataOne[[3]]
-# Q4 <- quartile_dataOne[[4]]
+saveQuartileFile(topHalf_Peaks)
+saveQuartileFile(topThird_Peaks)
+saveQuartileFile(topFourth_Peaks)
+saveQuartileFile(topTwoThird_Peaks)
 
