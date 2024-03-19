070d95d03b05e202169613b5adb54bb84ac00b51
RaodahAbod <raodah.abod@gmail.com>
2024-03-19 13:34:30 -0500

    Heatmap for H3K27me3 samples

--- Manual Heat Map.R
+++ Manual Heat Map.R
@@ -7,14 +7,11 @@ library(readr)
 
 # load in go enrich tibbles ---------------------------------------------------------
 
-ipsc_ENCODE_enrichGO <- read_csv(choose.files())
-Day0_enrichGO <- read_csv(choose.files())
-Day15_enrichGO <- read_csv(choose.files())
-Day30_enrichGO <- read_csv(choose.files())
-lv_ENCODE_enrichGO <- read_csv(choose.files())
-H21792_acc_enrichGO <- read_csv(choose.files())
-ROA_scr_enrichGO <- read.csv(choose.files())
-ROA_acc_enrichGO <- read_csv(choose.files())
+H3K27me3_Accutase <- read_csv(choose.files())
+H3K27me3_Trypsin <- read_csv(choose.files())
+H3K27me3_FlashFroz <- read_csv(choose.files())
+H3K27me3_Scraped <- read_csv(choose.files())
+
 
 # Building off the Cardiac Only Terms. Top 15 for Each. Except iPSC ---------------------
 # this works. this is what i use. 
@@ -28,41 +25,28 @@ sortAndExtractHeartOnly <- function(dataSet){
  return(dataSet)
 }
 
-newDay0 <- dplyr::select(Day0_enrichGO, Description, `Day 0 p.adjust` = `p.adjust`)
-newDay15 <- dplyr::select(Day15_enrichGO, Description,`Day 15 p.adjust` = `p.adjust`)
-newDay30 <- dplyr::select(Day30_enrichGO, Description, `Day 30 p.adjust` = `p.adjust`)
-newENCODELV <- dplyr::select(lv_ENCODE_enrichGO, Description, `ENCODE p.adjust` = `p.adjust`)
-newENCODEiPS <- dplyr::select(ipsc_ENCODE_enrichGO, Description, `ENCODE p.adjust` = `p.adjust`)
-newROAscr <- dplyr::select(ROA_scr_enrichGO, Description, `ENCODE p.adjust` = `p.adjust`)
-newROAacc <- dplyr::select(ROA_acc_enrichGO, Description, `ENCODE p.adjust` = `p.adjust`)
-newH21792acc <- dplyr::select(H21792_acc_enrichGO, Description, `ENCODE p.adjust` = `p.adjust`)
+newAccutase <- dplyr::select(H3K27me3_Accutase, Description, `Day 0 p.adjust` = `p.adjust`)
+newTrypsin <- dplyr::select(H3K27me3_Trypsin, Description,`Day 15 p.adjust` = `p.adjust`)
+newFlashFroz <- dplyr::select(H3K27me3_FlashFroz, Description, `Day 30 p.adjust` = `p.adjust`)
+newScraped <- dplyr::select(H3K27me3_Scraped, Description, `ENCODE p.adjust` = `p.adjust`)
 
-
-topTermsOnly <- rbind(sortAndExtractHeartOnly(Day0_enrichGO),
-                      sortAndExtractHeartOnly(Day15_enrichGO),
-                      sortAndExtractHeartOnly(Day30_enrichGO),
-                      sortAndExtractHeartOnly(lv_ENCODE_enrichGO),
-                      sortAndExtractHeartOnly(ipsc_ENCODE_enrichGO),
-                      sortAndExtractHeartOnly(ROA_scr_enrichGO),
-                      sortAndExtractHeartOnly(ROA_acc_enrichGO),
-                      sortAndExtractHeartOnly(H21792_acc_enrichGO)) %>%
+topTermsOnly <- rbind(sortAndExtractHeartOnly(H3K27me3_Accutase),
+                      sortAndExtractHeartOnly(H3K27me3_Trypsin),
+                      sortAndExtractHeartOnly(H3K27me3_FlashFroz),
+                      sortAndExtractHeartOnly(H3K27me3_Scraped)) %>%
   dplyr::select("Description")
 
 masterTopHeartGOs <- topTermsOnly %>% 
-  left_join(newDay0, join_by(Description)) %>% 
-  left_join(newDay15, join_by(Description)) %>% 
-  left_join(newDay30, join_by(Description)) %>% 
-  left_join(newENCODELV, join_by(Description)) %>%
-  left_join(newENCODEiPS, join_by(Description)) %>%
-  left_join(newROAscr, join_by(Description)) %>%
-  left_join(newROAacc, join_by(Description)) %>%
-  left_join(newH21792acc, join_by(Description)) %>%
+  left_join(newAccutase, join_by(Description)) %>% 
+  left_join(newTrypsin, join_by(Description)) %>% 
+  left_join(newFlashFroz, join_by(Description)) %>% 
+  left_join(newScraped, join_by(Description)) %>%
   unique()
 
-colnames(masterTopHeartGOs) <- c("Description","Day 0 H3K27ac", "Day 15 H3K27ac", 
-                                 "Day 30 H3K27ac", "ENCODE LV H3K27ac", 
-                                 "ENCODE iPS H3K27ac",
-                                 "ROA 78-1 Scr", 'ROA 78-1 Acc', 'H21792 Acc')                           
+colnames(masterTopHeartGOs) <- c("Description","78-1 H3K27me3 Accutase", 
+                                 "78-1 H3K27me3 Trypsin", 
+                                 "78-1 H3K27me3 Flash Frozen",
+                                 "78-1 H3K27me3 Scraped")                           
 
 mth_terms <- masterTopHeartGOs$Description
 masterTopHeartGOs_noID <- subset(masterTopHeartGOs, select = -c(Description))
