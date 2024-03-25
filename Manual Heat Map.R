d4b95eb631670b92cf6f37f3d721f09d0a35332f
RaodahAbod <raodah.abod@gmail.com>
2024-03-25 12:34:29 -0500

    Identifies top general terms for H3K27me3 samples and also isolates terms with "development", "Morphogenesis", and "Differentiation"

--- Manual Heat Map.R
+++ Manual Heat Map.R
@@ -7,50 +7,51 @@ library(readr)
 
 # load in go enrich tibbles ---------------------------------------------------------
 
-H3K27ac_Accutase_H21792 <- read_csv(choose.files())
-H3K27ac_Trypsin_H21792 <- read_csv(choose.files())
-H3K27ac_Accutase_78_1 <- read_csv(choose.files())
-H3K27ac_Scraped_78_1 <- read_csv(choose.files())
-H3K27ac_ENCODE_LV <- read_csv(choose.files())
+H3K27me3_Accutase <- read_csv(choose.files())
+H3K27me3_Trypsin <- read_csv(choose.files())
+H3K27me3_FlashFroz <- read_csv(choose.files())
+H3K27me3_Scraped <- read_csv(choose.files())
+H3K27me3_ENCODE <- read_csv(choose.files())
 
 # Building off the Cardiac Only Terms. Top 15 for Each. Except iPSC ---------------------
 # this works. this is what i use. 
 
 sortAndExtractHeartOnly <- function(dataSet){ 
- dataSet <- dataSet %>%
-  filter(grepl("cardi|heart", Description, ignore.case = TRUE,)) %>%
-  arrange(p.adjust) %>%
-  slice(1:15) %>%
-  select(Description, p.adjust)
- return(dataSet)
+  dataSet <- dataSet %>%
+    filter(grepl("development|differentiation|morphogenesis",
+                 Description, ignore.case = TRUE,)) %>%
+    arrange(p.adjust) %>%
+    slice(1:15) %>%
+    select(Description, p.adjust)
+  return(dataSet)
 }
 
-newAccutase_H21792 <- dplyr::select(H3K27ac_Accutase_H21792, Description, `Sample 1 p.adjust` = `p.adjust`)
-newTrypsin_H21792 <- dplyr::select(H3K27ac_Trypsin_H21792, Description,`Sample 2 p.adjust` = `p.adjust`)
-newAccutase_781 <- dplyr::select(H3K27ac_Accutase_78_1, Description, `Sample 3 p.adjust` = `p.adjust`)
-newScraped_781 <- dplyr::select(H3K27ac_Scraped_78_1, Description, `Sample 4 p.adjust` = `p.adjust`)
-newEncode <- dplyr::select(H3K27ac_ENCODE_LV, Description, `Sample 5 p.adjust` = `p.adjust`)
+newAccutase <- dplyr::select(H3K27me3_Accutase, Description, `Sample 1 p.adjust` = `p.adjust`)
+newTrypsin <- dplyr::select(H3K27me3_Trypsin, Description,`Sample 2 p.adjust` = `p.adjust`)
+newFlashFroz <- dplyr::select(H3K27me3_FlashFroz, Description, `Sample 3 p.adjust` = `p.adjust`)
+newScraped <- dplyr::select(H3K27me3_Scraped, Description, `Sample 4 p.adjust` = `p.adjust`)
+newENCODE <- dplyr::select(H3K27me3_ENCODE, Description, `Sample 5 p.adjust` = `p.adjust`)
 
-topTermsOnly <- rbind(sortAndExtractHeartOnly(H3K27ac_Accutase_H21792),
-                      sortAndExtractHeartOnly(H3K27ac_Trypsin_H21792),
-                      sortAndExtractHeartOnly(H3K27ac_Accutase_78_1),
-                      sortAndExtractHeartOnly(H3K27ac_Scraped_78_1),
-                      sortAndExtractHeartOnly(H3K27ac_ENCODE_LV)) %>%
+topTermsOnly <- rbind(sortAndExtractHeartOnly(H3K27me3_Accutase),
+                      sortAndExtractHeartOnly(H3K27me3_Trypsin),
+                      sortAndExtractHeartOnly(H3K27me3_FlashFroz),
+                      sortAndExtractHeartOnly(H3K27me3_Scraped),
+                      sortAndExtractHeartOnly(H3K27me3_ENCODE)) %>%
   dplyr::select("Description")
 
 masterTopHeartGOs <- topTermsOnly %>% 
-  left_join(newAccutase_H21792, join_by(Description)) %>% 
-  left_join(newTrypsin_H21792, join_by(Description)) %>% 
-  left_join(newAccutase_781, join_by(Description)) %>% 
-  left_join(newScraped_781, join_by(Description)) %>%
-  left_join(newEncode, join_by(Description)) %>%
+  left_join(newAccutase, join_by(Description)) %>% 
+  left_join(newTrypsin, join_by(Description)) %>% 
+  left_join(newFlashFroz, join_by(Description)) %>% 
+  left_join(newScraped, join_by(Description)) %>%
+  left_join(newENCODE, join_by(Description)) %>%
   unique()
 
-colnames(masterTopHeartGOs) <- c("Description","H21792 H3K27ac Accutase", 
-                                 "H21792 H3K27ac Trypsin", 
-                                 "78-1 H3K27ac Accutase",
-                                 "78-1 H3K27ac Scraped",
-                                 "LV H3K27ac ENCODE")                           
+colnames(masterTopHeartGOs) <- c("Description","78-1 H3K27me3 Accutase", 
+                                 "78-1 H3K27me3 Trypsin", 
+                                 "78-1 H3K27me3 Flash Frozen",
+                                 "78-1 H3K27me3 Scraped",
+                                 "LV ENCODE H3K27me3")                           
 
 mth_terms <- masterTopHeartGOs$Description
 masterTopHeartGOs_noID <- subset(masterTopHeartGOs, select = -c(Description))
@@ -61,4 +62,57 @@ logTopHeartGOs <- -log10(masterTopHeartGOs_noID)
 value_colors <- c("NA" = "gray")
 htmp <- Heatmap(as.matrix(logTopHeartGOs), na_col = value_colors)
 htmp@row_names_param[["gp"]][["fontsize"]] <- 8
-draw(htmp,column_title = "Heatmap of Top 15 Terms Containing `Heart` or `Cardiac`")
+draw(htmp,column_title = "Heatmap of Top Terms Containing 
+`Development`,`Differentiation`, or `Morphogenesis`")
+
+# For top General Terms Only ---------------------
+# this works. this is what i use. 
+
+sortAndExtract <- function(dataSet){ 
+  dataSet <- dataSet %>%
+    #filter(grepl("development|differentiation|morphogenesis",
+    #            Description, ignore.case = TRUE,)) %>%
+    arrange(p.adjust) %>%
+    slice(1:20) %>%
+    select(Description, p.adjust)
+  return(dataSet)
+}
+
+# newAccutase <- dplyr::select(H3K27me3_Accutase, Description, `Sample 1 p.adjust` = `p.adjust`)
+# newTrypsin <- dplyr::select(H3K27me3_Trypsin, Description,`Sample 2 p.adjust` = `p.adjust`)
+# newFlashFroz <- dplyr::select(H3K27me3_FlashFroz, Description, `Sample 3 p.adjust` = `p.adjust`)
+# newScraped <- dplyr::select(H3K27me3_Scraped, Description, `Sample 4 p.adjust` = `p.adjust`)
+# newENCODE <- dplyr::select(H3K27me3_ENCODE, Description, `Sample 5 p.adjust` = `p.adjust`)
+
+topGenTermsOnly <- rbind(sortAndExtract(H3K27me3_Accutase),
+                      sortAndExtract(H3K27me3_Trypsin),
+                      sortAndExtract(H3K27me3_FlashFroz),
+                      sortAndExtract(H3K27me3_Scraped),
+                      sortAndExtract(H3K27me3_ENCODE)) %>%
+  dplyr::select("Description")
+
+masterTopGOs <- topGenTermsOnly %>% 
+  left_join(newAccutase, join_by(Description)) %>% 
+  left_join(newTrypsin, join_by(Description)) %>% 
+  left_join(newFlashFroz, join_by(Description)) %>% 
+  left_join(newScraped, join_by(Description)) %>%
+  left_join(newENCODE, join_by(Description)) %>%
+  unique()
+
+colnames(masterTopGOs) <- c("Description","78-1 H3K27me3 Accutase", 
+                                 "78-1 H3K27me3 Trypsin", 
+                                 "78-1 H3K27me3 Flash Frozen",
+                                 "78-1 H3K27me3 Scraped",
+                                 "LV ENCODE H3K27me3")                           
+
+masterGen_terms <- masterTopGOs$Description
+masterTopGOs_noID <- subset(masterTopGOs, select = -c(Description))
+masterTopGOs_noID <- as.data.frame(masterTopGOs_noID)
+rownames(masterTopGOs_noID) <- masterGen_terms
+logTopGOs <- -log10(masterTopGOs_noID)
+
+value_colors <- c("NA" = "gray")
+htmp_gen <- Heatmap(as.matrix(logTopGOs), na_col = value_colors)
+htmp_gen@row_names_param[["gp"]][["fontsize"]] <- 10
+draw(htmp_gen,column_title = "Heatmap of Top Terms Containing 
+`Development`,`Differentiation`, or `Morphogenesis`")
