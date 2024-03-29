7d2cbb7b7d921b490024a6f1f0aca4a7728726e2
RaodahAbod <raodah.abod@gmail.com>
2024-03-29 12:32:31 -0500

    included original peak calling to check for promoter region population in the H3K27ac samples

--- ChIP_seeker.R
+++ ChIP_seeker.R
@@ -16,24 +16,19 @@ loadFile_peakCall <- function(){
 }
 
 #calls function loadFile - stores as peak file
-chimpFile <- loadFile_peakCall()
+sample1 <- loadFile_peakCall()
+sample2 <- loadFile_peakCall()
+sample3 <- loadFile_peakCall()
+sample4 <- loadFile_peakCall()
+sample5 <- loadFile_peakCall()
+sample6 <- loadFile_peakCall()
+sample10 <- loadFile_peakCall()
+sample11 <- loadFile_peakCall()
 
-Day0_IgG_H21792 <- loadFile_peakCall()
-
-Day0_iPSC_H3K27ac <- loadFile_peakCall()
-Day0_iPSC_H3K9me3 <- loadFile_peakCall()
-Day15_CM_H3K27ac <- loadFile_peakCall()
-Day15_CM_H3K9me3 <- loadFile_peakCall()
-Day30_CM_H3K27ac <- loadFile_peakCall()
-
-ENCODE_iPSC20b_H3K27ac <- loadFile_peakCall()
-ENCODE_LV_H3K27ac <- loadFile_peakCall()
-
-Roadmap_iPSC20b_H3K27ac <- loadFile_peakCall()
-Roadmap_iPSC20b_H3K9me3 <- loadFile_peakCall()
-Roadmap_LV_H3K27ac <- loadFile_peakCall()
-Roadmap_LV_H3K9me3 <- loadFile_peakCall()
-
+sample5_old <- loadFile_peakCall()
+sample6_old <- loadFile_peakCall()
+sample10_old <- loadFile_peakCall()
+sample11_old <- loadFile_peakCall()
 
 # Protocol 1: ChIPseeker and epigenomic dataset prep ------------------------------------------
 
@@ -47,66 +42,53 @@ prepGRangeObj <- function(seek_object){
 }
 
 #calls the prepGrangeObj function and overwrites original peakfile variable
-chimpFile <- prepGRangeObj(chimpFile)
+sample1 <- prepGRangeObj(sample1)
+sample2 <- prepGRangeObj(sample2)
+sample3 <- prepGRangeObj(sample3)
+sample4 <-prepGRangeObj(sample4)
+sample5 <- prepGRangeObj(sample5)
+sample6 <- prepGRangeObj(sample6)
+sample10 <- prepGRangeObj(sample10)
+sample11 <- prepGRangeObj(sample11)
 
-Day0_IgG_H21792 <- prepGRangeObj(Day0_IgG_H21792)
-
-Day0_iPSC_H3K27ac <- Day0_iPSC_H3K27ac %>% prepGRangeObj()
-Day0_iPSC_H3K9me3 <- Day0_iPSC_H3K9me3 %>% prepGRangeObj()
-Day15_CM_H3K27ac <- Day15_CM_H3K27ac %>% prepGRangeObj()
-Day15_CM_H3K9me3 <- Day15_CM_H3K9me3 %>% prepGRangeObj()
-Day30_CM_H3K27ac <- prepGRangeObj(Day30_CM_H3K27ac)
-
-ENCODE_iPSC20b_H3K27ac <- prepGRangeObj(ENCODE_iPSC20b_H3K27ac)
-ENCODE_LV_H3K27ac <- prepGRangeObj(ENCODE_LV_H3K27ac)
-
-Roadmap_iPSC20b_H3K27ac <- prepGRangeObj(Roadmap_iPSC20b_H3K27ac)
-Roadmap_iPSC20b_H3K9me3 <- prepGRangeObj(Roadmap_iPSC20b_H3K9me3)
-Roadmap_LV_H3K27ac <- prepGRangeObj(Roadmap_LV_H3K27ac)
-Roadmap_LV_H3K9me3 <- prepGRangeObj(Roadmap_LV_H3K9me3)
+sample5_old <- prepGRangeObj(sample5_old)
+sample6_old <- prepGRangeObj(sample6_old)
+sample10_old <- prepGRangeObj(sample10_old)
+sample11_old <- prepGRangeObj(sample11_old)
 
 # Protocol 2: Annotation of Epigenomic datasets----------------------------------------------------------
 
 #loads in reference genomes used to align peak files
-#library("TxDb.Hsapiens.UCSC.hg38.knownGene")
-library("TxDb.Ptroglodytes.UCSC.panTro4.refGene")
-#library("org.Hs.eg.db")
-library("org.Pt.eg.db")
-pantro <- getGEOInfo(genome="pantro 4", simplify=TRUE)
+library("TxDb.Hsapiens.UCSC.hg38.knownGene")
+#library("TxDb.Ptroglodytes.UCSC.panTro4.refGene")
+library("org.Hs.eg.db")
+#library("org.Pt.eg.db")
+
+#pantro <- getGEOInfo(genome="pantro 4", simplify=TRUE)
 
 #creates a variable of the loaded reference genome for use in ChIPseeker functions
-#TxDb_hg38 = TxDb.Hsapiens.UCSC.hg38.knownGene
-Tx_Pantro4 = TxDb.Ptroglodytes.UCSC.panTro4.refGene
+TxDb_hg38 = TxDb.Hsapiens.UCSC.hg38.knownGene
+#Tx_Pantro4 = TxDb.Ptroglodytes.UCSC.panTro4.refGene
 
 #Stores a concatenated list of peak files. user specified, order and name of peaks will be 
 #the new name of peaks in the list and will be used in figure outputs. 
-Epi_list <- GRangesList(chimpD15H3K27ac = chimpFile)
+Epi_list <- GRangesList(`78-1 H3K27me3 Accutase` = sample1, 
+                        `78-1 H3K27me3 Trypsin` = sample2,
+                        `78-1 H3K27me3 Flash Froz` = sample3,
+                        `78-1 H3K27me3 Scraped` = sample4,
+                        `H21792 H3K27ac Accutase` = sample5,
+                        `H21792 H3K27ac Trypsin` = sample6,
+                        `78-1 H3K27ac Accutase` = sample10,
+                        `78-1 H3K27ac Scraped` = sample11) 
 
-#GRanges(originalGRangeObj = newDataName) <-- setup of GRangesList input
-Epi_list_k27 <- GRangesList(Day0_iPSC_H3K27ac = Day0_iPSC_H3K27ac, 
-                            Day15_CM_H3K27ac = Day15_CM_H3K27ac, 
-                            Day30_CM_H3K27ac = Day30_CM_H3K27ac, 
-                            ENCODE_iPSC20b_H3K27ac = ENCODE_iPSC20b_H3K27ac,
-                            ENCODE_LV_H3K27ac = ENCODE_LV_H3K27ac,
-                            Roadmap_iPSC20b_H3K27ac = Roadmap_iPSC20b_H3K27ac,
-                            Roadmap_LV_H3K27ac = Roadmap_LV_H3K27ac)
+old_Epi_list <- GRangesList(`H21792 H3K27ac Accutase (orig Broad M2 peaks)` = sample5_old,
+                            `H21792 H3K27ac Trypsin (orig Broad M2 peaks)` = sample6_old,
+                            `78-1 H3K27ac Accutase (orig Broad M2 peaks)` = sample10_old,
+                            `78-1 H3K27ac Scraped (orig Broad M2 peaks)` = sample11_old)
 
-Epi_list_k9 <- GRangesList(Day0_iPSC_H3K9me3 = Day0_iPSC_H3K9me3,
-                           Day15_CM_H3K9me3 = Day15_CM_H3K9me3,
-                           Roadmap_iPSC20b_H3K9me3 = Roadmap_iPSC20b_H3K9me3,
-                           Roadmap_LV_H3K9me3 = Roadmap_LV_H3K9me3)
+peakAnnoList <- lapply(Epi_list, annotatePeak, tssRegion = c(-2000,2000),TxDb = TxDb_hg38)
 
-#peakAnnoList_k27 <- lapply(Epi_list_k27, annotatePeak, tssRegion = c(-2000,2000), 
-                       #TxDb = TxDb_hg38)
-#peakAnnoList_k9 <- lapply(Epi_list_k9, annotatePeak, tssRegion = c(-2000,2000), 
-                      # TxDb = TxDb_hg38)
-
-#function of ChIPseeker to annotate peak files within the list. 
-peakAnnoList <- lapply(Epi_list, annotatePeak, tssRegion = c(-2000,2000), 
-                       TxDb = pantro)
-
-peakAnnoList <- lapply(Epi_list, annotatePeak, tssRegion = c(-2000,2000), 
-                       TxDb = Tx_Pantro4)
+peakAnnoList2 <- lapply(old_Epi_list, annotatePeak, tssRegion = c(-2000,2000),TxDb = TxDb_hg38)
 
 #list2env(peakAnnoList,envir = .GlobalEnv) #renee!
 
@@ -117,27 +99,15 @@ library(clusterProfiler)
 library(DOSE)
 
 #plotting the annotation bar plot. Input is peakAnnoList
-plotAnnoBar(peakAnnoList_k27)
-plotAnnoBar(peakAnnoList_k9)
-
 plotAnnoBar(peakAnnoList)
-
+plotAnnoBar(peakAnnoList2)
 # Metaplots -------------------------------------------------------------------------
-
 
 #Function to generate the locations of transcription start sites in your reference genome.
 TSS = getBioRegion(TxDb=TxDb_hg38, upstream=3000, downstream=3000, by = "gene", 
                    type = "start_site")
-#TSS = getBioRegion(TxDb=Tx_Pantro4, upstream=3000, downstream=3000, by = "gene", 
-#                   type = "start_site")
 
-#tagging of peaks by transcription start site. input is Epi_list
-Epi_list_tagMatrix_k27 = lapply(Epi_list_k27, getTagMatrix, windows = TSS)
-Epi_list_tagMatrix_k9 = lapply(Epi_list_k9, getTagMatrix, windows = TSS)
+Epi_list_tagMatrix = lapply(Epi_list, getTagMatrix, windows = TSS)
 
 #plotting the transcription Start Site density meta plot.
-plotAvgProf(Epi_list_tagMatrix_k27, xlim=c(-3000, 3000), ylab = "Count Frequency")
-plotAvgProf(Epi_list_tagMatrix_k9, xlim=c(-3000, 3000), ylab = "Count Frequency")
-
-
-
+plotAvgProf(Epi_list_tagMatrix, xlim=c(-3000, 3000), ylab = "Count Frequency")
